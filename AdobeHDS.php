7650948d71a3313741378ea29038696b957e484f
KSV <faltuvisitor@yahoo.co.in>
2012-10-08 18:16:14 +0530

    use manifest hash as fragment name prefix + other improvements

--- AdobeHDS.php
+++ AdobeHDS.php
@@ -744,6 +744,10 @@
               $this->baseFilename = substr($this->baseFilename, strrpos($this->baseFilename, '/') + 1) . "Seg" . $segNum . "-Frag";
           else
               $this->baseFilename .= "Seg" . $segNum . "-Frag";
+          if (strpos($manifest, "?"))
+              $this->baseFilename = md5(substr($manifest, 0, strpos($manifest, "?"))) . "_" . $this->baseFilename;
+          else
+              $this->baseFilename = md5($manifest) . "_" . $this->baseFilename;
 
           if ($fragNum >= $this->fragCount)
               LogError("No fragment available for downloading");
@@ -1389,6 +1393,18 @@
       return $outPath;
     }
 
+  function RemoveExtension($outFile)
+    {
+      preg_match("/\.\w{1,3}$/i", $outFile, $extension);
+      if (isset($extension[0]))
+        {
+          $extension = $extension[0];
+          $outFile   = substr($outFile, 0, -strlen($extension));
+          return $outFile;
+        }
+      return $outFile;
+    }
+
   function ShowHeader()
     {
       $header = "KSV Adobe HDS Downloader";
@@ -1588,8 +1604,9 @@
         }
     }
 
-  if ($outFile and (substr($outFile, -4) == ".flv"))
-      $outFile = substr($outFile, 0, -4);
+  // Remove existing file extension
+  if ($outFile)
+      $outFile = RemoveExtension($outFile);
 
   // Disable filesize when piping
   if ($play)
@@ -1612,7 +1629,7 @@
   if (!$outFile)
     {
       $baseFilename = str_replace('\\', '/', $baseFilename);
-      if ($baseFilename and (substr($baseFilename, -1) != '/') and (substr($baseFilename, -1) != ':'))
+      if ($baseFilename and !((substr($baseFilename, -1) == '/') or (substr($baseFilename, -1) == ':')))
         {
           if (strrpos($baseFilename, '/'))
               $outFile = substr($baseFilename, strrpos($baseFilename, '/') + 1);
@@ -1621,6 +1638,7 @@
         }
       else
           $outFile = "Joined";
+      $outFile = RemoveExtension($outFile);
     }
 
   // Check for available fragments and rename if required

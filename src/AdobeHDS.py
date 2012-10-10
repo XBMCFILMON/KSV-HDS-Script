a3cc816440e7aa7e15817a4b7c5362a6488f0e69
Jonathan Prince <princej.ws01@hotmail.co.uk>
2012-10-10 21:42:24 +0100

    Initial empty python conversion file

---
+++ src/AdobeHDS.py
@@ -0,0 +1,17 @@
+'''
+Python conversion of AdobeHDS.php written by K-S-V
+    original is available at https://github.com/K-S-V/Scripts
+    
+Initially this is intended to be a 1-1 conversion
+'''
+class CLI:
+    def displayHelp(self):
+        print 'Hello world'
+
+class cURL:
+    def headers(self):
+        return ["One", "two", "Three"]
+
+class F4F:
+    def InitDecoder(self):
+        pass
